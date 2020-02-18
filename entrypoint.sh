fec3b06d0a418de45e6b08d57456a1803761ab40
Ryan Frizzell <ryan-frizzell@cirrusmd.com>
2020-02-18 13:29:42 -0700

    profit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 set -eo
-ls -al
-env
 docker login -u ${docker_username} -p ${docker_password}
 docker build -t tmpimage -f ${dockerfile} .
-docker tag tmpimage ${docker_registry}
-docker push  ${docker_registry}
+docker tag tmpimage ${docker_registry}:latest
+docker push  ${docker_registry}:latest
 docker image ls tmpimage -q
