28539677d0262a5e18c96f35d47bf7718ae9d670
Ryan Frizzell <ryan-frizzell@cirrusmd.com>
2020-02-18 13:48:13 -0700

    more profit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 set -eo
-docker login -u ${docker_username} -p ${docker_password}
+env
+if [[ -e docker_registry_repo ]]; then
+  docker_registry="${docker_registry_url}/${docker_registry_repo}"
+else
+  docker_registry="${docker_registry_url}"
+fi
+docker_image="${docker_registry}/${docker_image}"
+docker login ${docker_registry_url} -u ${docker_username} -p ${docker_password}
 docker build -t tmpimage -f ${dockerfile} .
-docker tag tmpimage ${docker_registry}:latest
-docker push  ${docker_registry}:latest
+docker tag tmpimage ${docker_image}:latest
+docker push  ${docker_image}:latest
 docker image ls tmpimage -q
