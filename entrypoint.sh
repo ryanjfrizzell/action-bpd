b2a0af3b1f3a232ff01da620b3506ca9895d2071
Ryan Frizzell <ryan-frizzell@cirrusmd.com>
2020-02-18 15:00:56 -0700

    stash this for now

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ else
   docker_registry="${docker_registry_url}"
 fi
 docker_image="${docker_registry}/${docker_image}"
-docker login ${docker_registry_url} -u ${docker_username} -p ${docker_password}
+#docker login ${docker_registry_url} -u ${docker_username} -p ${docker_password}
 docker build -t tmpimage -f ${dockerfile} .
 docker tag tmpimage ${docker_image}:latest
 docker push  ${docker_image}:latest
