88a54f6902591573076b303d84d7afd4fb926344
Ryan Frizzell <ryan-frizzell@cirrusmd.com>
2020-02-19 07:59:47 -0700

    more stuff

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 set -exo
-env
 if [[ -e docker_registry_repo ]]; then
   docker_registry="${docker_registry_url}/${docker_registry_repo}"
 else
   docker_registry="${docker_registry_url}"
 fi
 docker_image="${docker_registry}/${docker_image}"
+echo "docker image set to ${docker_image}"
 #docker login ${docker_registry_url} -u ${docker_username} -p ${docker_password}
+echo "docker build -t tmpimage -f ${dockerfile} ."
 docker build -t tmpimage -f ${dockerfile} .
 docker tag tmpimage ${docker_image}:latest
 docker push  ${docker_image}:latest
