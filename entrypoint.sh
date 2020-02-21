0a1b4ccf7e981df05bbd25d249f667940f8d9c5e
Ryan Frizzell <ryan-frizzell@cirrusmd.com>
2020-02-21 11:05:21 -0700

    lets try this...

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,23 +8,25 @@ docker_username=$5
 docker_password=$6
 dockerfile=$7
 docker_image_tag=$8
+docker_registry=$9
 
 
 # otherwise we try to assemble your docker image based on gihub's repo standard
 # https://help.github.com/en/packages/using-github-packages-with-your-projects-ecosystem/configuring-docker-for-use-with-github-packages
-if [[ -z docker_image ]]; then
-    docker_registry_image="${docker_registry_url}/${docker_registry_owner}/${docker_repository}"
-else
+if [[ -z docker_registry ]]; then
     docker_registry_image="${docker_registry_url}/${docker_registry_owner}/${docker_repository}/${docker_image}"
+else
+    docker_registry_image="${docker_registry_url}/${docker_registry_owner}/${docker_repository}"
 fi
 tagged_image="${docker_registry_image}:${docker_image_tag}"
+echo "docker image set to ${docker_registry_image}"
+echo "using tagged image of ${tagged_image}"
+
 if [[ -z $docker_registry_image ]]; then
     echo 'we failed to assemble a docker registry/image combination'
     exit 3
 fi
-echo "docker image set to ${docker_registry_image}"
-echo "using tagged image of ${tagged_image}"
-# login to docker
+# go with the do
 echo $docker_password | docker login ${docker_registry_image} -u ${docker_username} --password-stdin
 echo "docker build -t ${tagged_image}  -f ${dockerfile} ."
 docker build -t ${tagged_image} -f ${dockerfile} .
