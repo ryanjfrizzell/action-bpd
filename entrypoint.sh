eff48324d48ffe4e4f1a41ffe3a9afe54d984ca6
Ryan Frizzell <ryan-frizzell@cirrusmd.com>
2020-02-21 08:50:17 -0700

    getting there

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,17 +10,15 @@ dockerfile=$6
 
 if [[ $# -ne 6 ]]; then
     echo "missing one or more arguments"
+    exit 3
 fi
-if [[ -e docker_registry_repo ]]; then
-  docker_registry="${docker_registry_url}/${docker_registry_repo}"
-else
-  docker_registry="${docker_registry_url}"
-fi
-docker_image="${docker_registry}/${docker_image}"
-echo "docker image set to ${docker_image}"
-#docker login ${docker_registry_url} -u ${docker_username} -p ${docker_password}
+
+docker_registry_image="${docker_registry_url}/${docker_registry_repo}/${docker_image}"
+
+echo "docker image set to ${docker_registry_image}"
+docker login ${docker_registry_url} -u ${docker_username} -p ${docker_password}
 echo "docker build -t tmpimage -f ${dockerfile} ."
 docker build -t tmpimage -f ${dockerfile} .
-docker tag tmpimage ${docker_image}:latest
-docker push  ${docker_image}:latest
+docker tag tmpimage ${docker_registry_image}:latest
+docker push  ${docker_registry_image}:latest
 docker image ls tmpimage -q
